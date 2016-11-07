cf283ef3b718e5c18b88bfe1f6a76c71a8e80278
Jack Wilsdon <jack.wilsdon@gmail.com>
2016-11-07 22:12:43 +0000

    Increment version to 1.0.6.dev0

--- plex_download/__init__.py
+++ plex_download/__init__.py
@@ -1,6 +1,6 @@
 from plex_download.client import DownloadClient
 from plex_download.version import DownloadablePlexVersion
 
-__version__ = '1.0.5'
+__version__ = '1.0.6.dev0'
 
 __all__ = ('DownloadClient', 'DownloadablePlexVersion')
